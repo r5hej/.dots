6c245b588152fe218f53d9d432c5314c0e4d6334
r5hej <charliebyrdam@gmail.com>
2017-08-06 14:17:39 +0200

    Removed custom.el. Added function to create if not exists.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -7,7 +7,11 @@
 
 (setq vc-follow-symlinks t)                                               ; Follow symbolic links
 
+(defconst custom-file (expand-file-name "custom.el" user-emacs-directory))
+(unless (file-exists-p custom-file)
+  (write-region "" "" custom-file))
+
+(load custom-file)
+
 (org-babel-load-file "~/.emacs.d/configuration.org")
 
-(setq custom-file "~/.emacs.d/custom.el")
-(load custom-file)
