dc43964dc7a1b82c2cfda4a8ab296cc7b6acea37
r5hej <charliebyrdam@gmail.com>
2019-06-25 10:57:54 +0200

    Added rm!, which is rm but inverted

--- ranger/commands.py
+++ ranger/commands.py
@@ -3,8 +3,19 @@ from ranger.api.commands import Command
 import os
 import shutil
 import stat
+import subprocess
 
 home = os.path.expanduser('~')
+os.environ['RANGER_BOX_STATE'] = 'inactive'
+
+
+def set_env_var(var: str, state: str):
+    os.environ[var] = state
+
+
+def get_env_var(var: str):
+    return os.environ.get(var)
+
 
 class new_dot_script(Command):
     """:new_dot_script <filename>
@@ -18,7 +29,7 @@ class new_dot_script(Command):
             self.fm.notify('Script already exists')
             return
 
-        shutil.copy('{}/.dots/bin/.template'.format(home) , f)
+        shutil.copy('{}/.dots/bin/.template'.format(home), f)
         os.chmod(f, os.stat(f).st_mode | stat.S_IEXEC)
         self.fm.edit_file(f)
 
@@ -60,3 +71,31 @@ class unzip(Command):
             return
 
         os.system('unzip {} > /dev/null 2>&1'.format(self.fm.thisfile))
+
+
+class box(Command):
+    """:box
+
+    """
+
+    def execute(self):
+        state = get_env_var('RANGER_BOX_STATE')
+        batch_file = '/files/media/storage2/download/batch'
+
+        if state == 'active':
+            set_env_var('RANGER_BOX_STATE', 'inactive')
+
+            with open(batch_file, 'w') as f:
+                for line in self.fm.thistab.get_selection():
+                    fix_path = ''.join(line.realpath.split('/')[4:]).replace(' ', '\ ')
+                    f.write('get -r {} /files/media/storage2/download/\n'.format(fix_path))
+                 
+            self.fm.edit_file(batch_file)
+            os.system('sudo umount {}/mnt'.format(home))
+            #subprocess.Popen(['sftp', '-b', '/files/media/storage2/download/batch', 'box:/home/normarken/downloads'])
+            self.fm.cd(home)
+
+        else:
+            set_env_var('RANGER_BOX_STATE', 'active')
+            os.system('sshfs r5hej@dsb33542.seedbox.io:/home/normarken/downloads $HOME/mnt/')
+            self.fm.cd(home + '/mnt')
