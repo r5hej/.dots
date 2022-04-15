0527539a52dede922869608582b40f2ed3a1ef6d
Charlie Byrdam <charliebyrdam@gmail.com>
2022-04-15 17:41:43 +0200

    Update commands.py

--- ranger/commands.py
+++ ranger/commands.py
@@ -6,7 +6,6 @@ import stat
 import subprocess
 
 home = os.path.expanduser('~')
-os.environ['RANGER_BOX_STATE'] = 'inactive'
 
 
 def set_env_var(var: str, state: str):
@@ -71,31 +70,3 @@ class unzip(Command):
             return
 
         os.system('unzip {} > /dev/null 2>&1'.format(self.fm.thisfile))
-
-
-class box(Command):
-    """:box
-
-    """
-
-    def execute(self):
-        state = get_env_var('RANGER_BOX_STATE')
-        batch_file = '/files/media/storage2/download/batch'
-
-        if state == 'active':
-            set_env_var('RANGER_BOX_STATE', 'inactive')
-
-            with open(batch_file, 'w') as f:
-                for line in self.fm.thistab.get_selection():
-                    fix_path = ''.join(line.realpath.split('/')[4:]).replace(' ', '\ ')
-                    f.write('get -r {} /files/media/storage2/download/\n'.format(fix_path))
-                 
-            self.fm.edit_file(batch_file)
-            os.system('sudo umount {}/mnt'.format(home))
-            #subprocess.Popen(['sftp', '-b', '/files/media/storage2/download/batch', 'box:/home/normarken/downloads'])
-            self.fm.cd(home)
-
-        else:
-            set_env_var('RANGER_BOX_STATE', 'active')
-            os.system('sshfs r5hej@dsb33542.seedbox.io:/home/normarken/downloads $HOME/mnt/')
-            self.fm.cd(home + '/mnt')
